5535e6b83c048234aa755d2c0691a6c0520161e2
Álvaro Torcato <alvaromanuelreis@gmail.com>
2022-11-15 17:39:03 +0000

    Initial commit

---
+++ src/main/java/com/example/voteq/VoteQApplication.java
@@ -0,0 +1,13 @@
+package com.example.voteq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteQApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoteQApplication.class, args);
+    }
+
+}
